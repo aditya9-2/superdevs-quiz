ac86b4ed6940820a903af940b688a450ba9e878b
Aditya <adityabasak7031@gmail.com>
2025-07-25 22:33:35 +0530

    add userProfile route

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import userSignup from "../controllers/user/signup.controller";
 import userSIgnin from "../controllers/user/signin.controller";
+import userProfile from "../controllers/user/userProfile.controller";
 
 
 const router = Router();
 
 router.post('/signup', userSignup);
 router.post('/signin', userSIgnin);
+router.get('/profile', userProfile)
 
 export default router
