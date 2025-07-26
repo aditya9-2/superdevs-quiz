bc13e6523e5faf3a6cd7df696e1365290cee46aa
Aditya <adityabasak7031@gmail.com>
2025-07-26 21:11:34 +0530

    add middleware to userProfile

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import userSignup from "../controllers/user/signup.controller";
 import userSIgnin from "../controllers/user/signin.controller";
 import userProfile from "../controllers/user/userProfile.controller";
+import authMiddleware from "../middleware/authMiddleware";
 
 
 const router = Router();
 
 router.post('/signup', userSignup);
 router.post('/signin', userSIgnin);
-router.get('/profile', userProfile)
+router.get('/profile', authMiddleware, userProfile)
 
 export default router
