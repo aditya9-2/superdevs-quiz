04b6780eee6419d0d56ad15b225c1abb781e4a24
Aditya <adityabasak7031@gmail.com>
2025-07-26 21:09:23 +0530

    add strong types

--- backend/src/types/customRequest.ts
+++ backend/src/types/customRequest.ts
@@ -1,6 +1,10 @@
+import { Role } from "@prisma/client";
 import { Request } from "express";
 import { JwtPayload } from "jsonwebtoken";
 
 export interface IGetUserAuthInfoRequest extends Request {
-    user?: string | JwtPayload
+    user?: JwtPayload & {
+        id: number;
+        role: Role;
+    };
 }
